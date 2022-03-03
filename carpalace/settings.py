3847b26e4aef86d05f148dae3bfc19dad5782e80
CoderParth <ghantigalli@gmail.com>
2022-03-04 03:53:32 +1100

    update

--- carpalace/settings.py
+++ carpalace/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['carsncars..herokuapp.com']
+ALLOWED_HOSTS = ['carsncars.herokuapp.com']
 
 
 # Application definition
